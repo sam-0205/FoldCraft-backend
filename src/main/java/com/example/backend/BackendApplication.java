e5caa0887da1c04dd6f95ae97442599632f80b16
Sameer Rath <Sameer.Rath@PROFINCHBLR.COM>
2026-04-03 16:53:02 +0530

    ev for render deployment

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -13,10 +13,12 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
+		if (System.getenv("RENDER") == null) {
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry -> {
+				System.setProperty(entry.getKey(), entry.getValue());
+			});
+		}
 
 		SpringApplication.run(BackendApplication.class, args);
 	}
